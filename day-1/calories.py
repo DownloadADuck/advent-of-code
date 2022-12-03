b194fd74515b5f51cd00c000e93cb9ef30839a3e
Luis Betran Alvarez <lalvarez@DESKTOP-MKNAANH.localdomain>
2022-12-03 16:37:01 +0100

    list is split in different Elves inventories. We now need to add each inventories

---
+++ day-1/calories.py
@@ -0,0 +1,8 @@
+# We need to find the Elf which carries the most calories. 
+
+log = open("/home/lalvarez/dev/advent-of-code/day-1/test_ds.log")
+
+elfs_inventories = log.read()
+elfs_inventories.split("\n\n")
+
+print(elfs_inventories.split("\n\n"))

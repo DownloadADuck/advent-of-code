58d6238054f41f8ff2c6fa54ebf5abd3af9bf12b
Luis Betran Alvarez <lalvarez@DESKTOP-MKNAANH.localdomain>
2022-12-03 17:56:53 +0100

    Found the 3 Elfs carrying the most calories

--- day-1/calories.py
+++ day-1/calories.py
@@ -18,5 +18,12 @@ for i in inventories:
     sums = sum([int(val) for val in temp])
     list_of_inventories.append(sums)
 
+print("Number of the Elf with the most calories in his backpack:")
 print(list_of_inventories.index(max(list_of_inventories))+1)
+print("Number of calories in said backpack:")
 print(max(list_of_inventories))
+
+biggest_calories = sum(sorted(list_of_inventories, reverse=True)[:3])
+
+print("Sum of the calories shared by the 3 Elfs carrying the most calories:")
+print(biggest_calories)

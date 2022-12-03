34ea6bb65d8c3e5ace5ec453d39d02f368155789
Luis Betran Alvarez <lalvarez@DESKTOP-MKNAANH.localdomain>
2022-12-03 17:48:19 +0100

    First part done, now to find the 3 Elfs that carry the most calories

--- day-1/calories.py
+++ day-1/calories.py
@@ -1,8 +1,22 @@
 # We need to find the Elf which carries the most calories. 
 
-log = open("/home/lalvarez/dev/advent-of-code/day-1/test_ds.log")
+#file = open("/home/lalvarez/dev/advent-of-code/day-1/test_ds.log")
+file = open("/home/lalvarez/dev/advent-of-code/day-1/real_ds.log")
 
-elfs_inventories = log.read()
-elfs_inventories.split("\n\n")
+inventories = file.read()
+inventories = inventories.split("\n\n")
 
-print(elfs_inventories.split("\n\n"))
+list_of_inventories = []
+
+for i in inventories:
+
+    temp = i.split("\n")
+    for k in temp:
+        if k == '':
+            temp.remove(k)
+
+    sums = sum([int(val) for val in temp])
+    list_of_inventories.append(sums)
+
+print(list_of_inventories.index(max(list_of_inventories))+1)
+print(max(list_of_inventories))
